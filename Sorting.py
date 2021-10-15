0f5516bbae1f1e7d984404b736003d3877605aac
Archer Murray <amurray@cub.uca.edu>
2021-10-15 11:29:19 -0500

    Created testing routine (work in progress)

--- Sorting.py
+++ Sorting.py
@@ -1,41 +1,50 @@
-from datetime import datetime
 import random
 import time
 
+"""
+Base class for all sorting algorithms.
+
+@author: Josh Hicks
+"""
 class Sort:
-    def __init__(self, args):
-        s = time.time()
-        a2 = []
-        a2.extend(args)
-        self.sort(a2)
-        print("Execution Time For",self.__class__.__name__.replace("_", " "),":", time.time() - s, "seconds for",len(a2),"elements")        
-    
+    def __init__(self):
+        pass
+
     def sort(self, args):
         pass
-    
+
     def isSorted(self, args):
-        lastVal = None
-        for i in args:
-            if lastVal:
-                if lastVal > i:
-                    #print("Not sorted")
-                    return False
-            lastVal = i
+        for i in range(len(args) - 1):
+            if args[i] > args[i + 1]:
+                return False
         return True
-        
-class Merge_Sort(Sort):
-    def sort(self,args):
+
+    def experimentSort(self, args):
+        t1 = time.perf_counter()
+        args = self.sort(args)
+        t2 = time.perf_counter()
+        if not self.isSorted(args):
+            return -1
+        return t2 - t1
+
+
+"""
+Implementation of merge sort.
+
+@author: Josh Hicks
+"""
+class MergeSort(Sort):
+    def sort(self, args):
         if(len(args) <= 1):
             return args
         middle = len(args) // 2
         left = args[:middle]
         right = args[middle:]
-
         left = self.sort(left)
         right = self.sort(right)
         return list(self.merge(left, right))
 
-    def merge(self,left, right):
+    def merge(self, left, right):
         result = []
         left_idx = 0
         right_idx = 0
@@ -48,14 +57,18 @@ class Merge_Sort(Sort):
                 right_idx += 1
         if left_idx < len(left):
             result.extend(left[left_idx:])
-            
         if right_idx < len(right):
             result.extend(right[right_idx:])
-            
-        return result 
+        return result
 
-class Quick_Sort(Sort):
-    def partition(self,array, begin, end):
+
+"""
+Implementation of quick sort.
+
+@author: Josh Hicks
+"""
+class QuickSort(Sort):
+    def partition(self, array, begin, end):
         pivot_idx = begin
         for i in range(begin+1, end+1):
             if array[i] <= array[begin]:
@@ -64,21 +77,25 @@ class Quick_Sort(Sort):
         array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
         return pivot_idx
 
-    def quick_sort_recursion(self,array, begin, end):
+    def quick_sort_recursion(self, array, begin, end):
         if begin >= end:
-            return 
+            return
         pivot_idx = self.partition(array, begin, end)
         self.quick_sort_recursion(array, begin, pivot_idx-1)
         self.quick_sort_recursion(array, pivot_idx+1, end)
-    
-    def sort(self,array, begin=0, end=None):
-        if end is None:
-            end = len(array) - 1 
-        self.quick_sort_recursion(array, begin, end)
-        return array
-            
-class Bubble_Sort(Sort):
-    def sort(self,args):
+
+    def sort(self, args):
+        self.quick_sort_recursion(args, 0, len(args) - 1)
+        return args
+
+
+"""
+Implementation of bubble sort.
+
+@author: Josh Hicks
+"""
+class BubbleSort(Sort):
+    def sort(self, args):
         swapped = True
         while swapped:
             swapped = False
@@ -90,7 +107,13 @@ class Bubble_Sort(Sort):
                     swapped = True
         return args
 
-class Insertion_Sort(Sort):
+
+"""
+Implementation of insertion sort.
+
+@author: Levi Lewis
+"""
+class InsertionSort(Sort):
     def sort(self, args):
         for i in range(len(args)):
             temp = args[i]
@@ -101,12 +124,26 @@ class Insertion_Sort(Sort):
             args[index] = temp
         return args
 
-max_length = 100000
-my_list = list(random.randint(0, 500) for i in range(max_length))  
-for j in range(0,max_length + 1, 1000):  
-    li = my_list[:j]
-    #Bubble_Sort(li)
-    #Merge_Sort(li)
-    #Quick_Sort(li)
-    Insertion_Sort(li)
-    print()
+
+"""
+Main testing routine (work in progress).
+
+@author: Archer Murray
+"""
+def test():
+    mst = MergeSort()
+    qst = QuickSort()
+    bst = BubbleSort()
+    ist = InsertionSort()
+    print(mst.experimentSort(
+        [random.randrange(1000) for i in range(1000)]))
+    print(qst.experimentSort(
+        [random.randrange(1000) for i in range(1000)]))
+    print(bst.experimentSort(
+        [random.randrange(1000) for i in range(1000)]))
+    print(ist.experimentSort(
+        [random.randrange(1000) for i in range(1000)]))
+
+
+if __name__ == '__main__':
+    test()
